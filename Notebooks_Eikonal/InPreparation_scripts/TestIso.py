5b072c606bfedbd48682b506caef96902ce32186
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2025-09-25 09:27:46 +0200

    Eikonal HFM testing

--- Notebooks_Eikonal/InPreparation_scripts/TestIso.py
+++ Notebooks_Eikonal/InPreparation_scripts/TestIso.py
@@ -2,12 +2,14 @@
 This file intends to illustrate the fast marching algorithm in the isotropic setting, and reproduce 
 some results of the agd implementation.
 """
-
-import numpy as np
-import taichi as ti
 import sys; sys.path.append("/Users/jean-mariemirebeau/Dropbox/Programmes/GithubM1/AGDT/AdaptiveGridDiscretizations_Taichi")
-import agdt
+sys.path.append("/Users/jean-mariemirebeau/Dropbox/Programmes/GithubM1/AdaptiveGridDiscretizations")
 from agdt.Eikonal import Metrics,HFM
+from agd.Plotting import quiver
+
+from matplotlib import pyplot as plt
+import taichi as ti
+import numpy as np
 np.set_printoptions(linewidth=2000)
 
 float_t = ti.f32; int_t = ti.i32
@@ -15,18 +17,80 @@ ti.init(arch=ti.cpu,default_fp=float_t,default_ip=int_t, debug=True)
 
 # Let us first build a domain, the rectangle [-1,1]x[0,1] discretized on a grid, and choose a metric type
 dom = HFM.Domain(bounds=[[-1,1],[0,1]], shape=(100,50), metric = Metrics.Diagonal(2,float_t))
+X = dom.grid()
 
 # We distinguish between physical points, which range over the rectangular domain, and indices, 
 # which range over the grid size
 print(f"{dom.IndexFromPoint((-0.5,0.3))=}")
 print(f"{dom.PointFromIndex((0.5,0.2))=}")
 
-# We then build the finite difference scheme. Some metric parameters, and walls, could be specified here.
-dom.build_scheme()
 
-# Set the seds for the front propagation
-dom.set_seed((-0.5,0.3))
-dom.set_seed((0.5,0.8),0.8)
+if False: # ---------------- Solving the eikonal equation in free space --------------
+    # We then build the finite difference scheme. Some metric parameters, and walls, could be specified here.
+    dom.build_scheme()
 
-# The eikonal equation can be solved using various numerical methods. Let us choose Fast Marching here
-dom.Algo.solve_FMM()
+    # Set the seds for the front propagation
+    dom.set_seed((-0.5,0.3)); dom.set_seed((0.5,0.8),0.8)
+
+    # The eikonal equation can be solved using various numerical methods. Let us choose Fast Marching here
+    dom.Algo.solve_FMM()
+    #dom.Algo.solve_AGSI()
+    #dom.Algo.solve_FastSweeping()
+    #dom.Algo.solve_GlobalIteration()
+
+    # Once the eikonal equation is solved, we compute the geodesics by solving a backtracking ode
+    ode = dom.ode()
+    geodesics,codes = ode.backtrack([[0.,0.6],[-0.9,0.5],[0.8,0.8]])
+    print(codes)
+
+    if False: # -------- Display the eikonal solution, and the geodesics ---------
+        plt.title("Numerical solution of the eikonal equation")
+        plt.contourf(*X,dom.values(True))
+        plt.axis('equal')
+        for geo in geodesics: plt.plot(*geo.T)
+        plt.show()
+
+    if False: # --------- Display the geodesic flow vector field ---------
+        plt.title("geodesic flow vector field")
+        quiver(*X,*np.moveaxis(ode.flows.to_numpy(),-1,0),subsampling=(2,2))
+        plt.axis('equal')
+        plt.show()
+
+# --------------- Introducing obstacles, and position dependent speed -----------
+X,Y = dom.sgrid()
+walls = np.logical_or((X-0.3)**2 + (Y-0.3)**2 <= 0.2**2, np.logical_and(X==X[35,0], Y>=0.4))
+costs = np.exp(-0.5*(X**2+Y**2))
+
+def to_field(arr,dtype): field = ti.field(dtype,arr.shape); field.from_numpy(arr); return field
+
+if False: # Run with obstacles and a cost function
+    dom.build_scheme(to_field(costs,float_t),to_field(walls,ti.i8))
+    dom.set_seed((-0.5,0.3)); dom.set_seed((0.5,0.8),0.5)
+
+    dom.Algo.solve_FMM()
+    geodesics,codes = dom.ode().backtrack([[0.,0.6],[-0.9,0.5],[0.8,0.8]])
+
+    plt.title("Numerical solution of the eikonal equation")
+    plt.contourf(*dom.grid(),dom.values(True),cmap='Greys')
+    plt.axis('equal')
+    for geo in geodesics: plt.plot(*geo.T)
+    plt.show()
+
+
+if True: # Custom stopping criterion
+    @ti.pyfunc
+    def stopping_criterion(ix):
+        # Here we stop the FMM at a prescribed distance.
+        # Alternatively, stop when all tips are accepted, etc
+        return dom.Algo.values[ix]>=0.7 
+
+    dom.build_scheme(to_field(costs,float_t),to_field(walls,ti.i8))
+    dom.set_seed((-0.5,0.3)); dom.set_seed((0.5,0.8),0.5)
+    dom.Algo.solve_FMM(stopping_criterion)
+    geodesics,codes = dom.ode().backtrack([[0.,0.6],[-0.9,0.5],[0.8,0.8]])
+
+    plt.title("Numerical solution of the eikonal equation")
+    plt.contourf(*dom.grid(),dom.values(True),cmap='Greys')
+    plt.axis('equal')
+    for geo in geodesics: plt.plot(*geo.T)
+    plt.show()
---
+++ Notebooks_Eikonal/InPreparation_scripts/TestRiemann.py
@@ -0,0 +1,120 @@
+"""
+This file intends to illustrate the fast marching algorithm in the isotropic setting, and reproduce 
+some results of the agd implementation.
+"""
+import sys; sys.path.append("/Users/jean-mariemirebeau/Dropbox/Programmes/GithubM1/AGDT/AdaptiveGridDiscretizations_Taichi")
+sys.path.append("/Users/jean-mariemirebeau/Dropbox/Programmes/GithubM1/AdaptiveGridDiscretizations")
+import agdt.AD, agdt.GetArrayModule
+from agdt.Eikonal import Metrics,HFM
+from agd.Plotting import quiver
+
+from matplotlib import pyplot as plt
+import taichi as ti
+import numpy as np
+np.set_printoptions(linewidth=2000)
+π = np.pi
+
+float_t = ti.f64; int_t = ti.i32
+ti.init(arch=ti.cpu,default_fp=float_t,default_ip=int_t, debug=True)
+
+bounds = [[-0.5,0.5],[-0.5,0.5]]
+dimx=101
+metricType = Metrics.Riemann(2,float_t)
+#dom = HFM.Domain(bounds,(5,5),metricType)
+
+dom = HFM.Domain(bounds,(dimx,dimx),metricType)
+riemann = ti.field(ti.math.mat2,dom.shape) 
+
+ti_fwd = agdt.AD.fwd_translator()
+@ti_fwd
+def elevation(x,y): return 0.75*np.sin(3*π*x)*np.sin(3*π*y)
+
+if True: # Build metric using autodiff
+    fwd1 = agdt.AD.mk_fwd1(2,float_t)
+
+    @ti.pyfunc
+    def topographic_metric(x,y):
+        x_ad = fwd1.types.mk(x,0)
+        y_ad = fwd1.types.mk(y,1)
+        h_ad = elevation(x_ad,y_ad)
+        grad = h_ad.v
+        return ti.math.eye(2) + grad.outer_product(grad)
+
+    @ti.kernel
+    def set_metric():
+        for index in ti.grouped(riemann):
+            p = dom.PointFromIndex(index)
+            riemann[index] = topographic_metric(p[0],p[1])
+    set_metric()
+if False: # Build metric using finite differences
+    Z = elevation.orig(*dom.grid())
+    DxZ,DyZ = np.gradient(Z,dom.h[0],axis=(0,1))
+    m = [[1+DxZ**2,DxZ*DyZ],[DxZ*DyZ,1+DyZ**2]]
+    riemann.from_numpy( np.moveaxis(m,(0,1),(-2,-1)) )
+if False: # Constant metric
+    riemann = agdt.GetArrayModule.tofield([[1,0],[0,1]],ti.math.mat2)
+
+tips = HFM.Domain(bounds,(6,6),metricType).grid()
+tips = np.moveaxis(tips,0,-1).reshape(-1,2)
+
+if True:
+    dom.build_scheme(m=riemann)
+    dom.set_seed((0,0))
+    dom.Algo.solve_FMM()
+
+
+    # Note : Some geodesics near the center do not look good (oscillate), but that is expected, since
+    # they start close to the cut locus
+    ode = dom.ode()
+    if True:
+        geodesics,codes = ode.backtrack(tips)
+        plt.contourf(*dom.grid(),dom.values(True),cmap='Greys')
+        for geo in geodesics: plt.plot(*geo.T)
+        plt.axis('equal')
+        plt.show()
+
+    if False:
+        quiver(*dom.grid(),*np.moveaxis(ode.flows.to_numpy(),-1,0),subsampling=(1,1))
+        plt.axis('equal')
+        plt.show()
+
+
+# ------ Reproducing the agd results ------
+from agd import Eikonal,Metrics
+Eikonal.LibraryCall.binary_dir['FileHFM']='/Users/jean-mariemirebeau/bin/FileHFM/Release'
+hfmIn = Eikonal.dictIn({
+    'model':'Riemann2',
+    'seed':[0.,0.],
+    'metric':Metrics.Riemann(np.moveaxis(riemann.to_numpy(),(-2,-1),(0,1))),
+    'tips':tips,
+    'exportValues':True,
+    'exportGeodesicFlow':True,
+    'geodesicSolver':'ODE'
+})
+hfmIn.SetRect(sides=bounds,dimx=dimx)
+hfmOut = hfmIn.Run()
+
+if False:
+    plt.contourf(*hfmIn.Grid(),hfmOut['values'])
+    for geo in hfmOut['geodesics']: plt.plot(*geo)
+    plt.axis('equal')
+    plt.show()
+
+if False:
+    quiver(*dom.grid(),*hfmOut['flow'],subsampling=(1,1))
+    plt.axis('equal')
+    plt.show()
+
+plt.contourf(*dom.grid(),hfmOut['values']-dom.values(True))
+plt.axis('equal')
+plt.colorbar()
+plt.show()
+
+#assert np.allclose(hfmOut['values'],dom.values(True),atol=1e-4)
+flow_agd = hfmOut['flow']
+flow_agdt = np.moveaxis(dom.flows()[0].to_numpy(),-1,0) #np.moveaxis(ode.flows.to_numpy(),-1,0)
+
+# Les flow diffèrent seulement en qq points, sur la diagonale. Pas forcément grave, 
+# cela correspond au cut-locus.
+#print(flow_agd+flow_agdt)
+#print(flow_agd)
